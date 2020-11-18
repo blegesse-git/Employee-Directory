cd844aba50195a4d404624a276bf3d503ed79b26
Beth Legesse <legessebl@gmail.com>
2020-11-18 17:02:29 -0600

    search result update

--- employee-app/src/components/SearchResults/index.js
+++ employee-app/src/components/SearchResults/index.js
@@ -4,14 +4,84 @@ import "./style.css";
 function SearchResults(props) {
   return (
     <div >
-        <ul className="list-group list-group-horizontal-xl" id="title">
-            <li className="list-group-item flex-fill">Image</li>
-            <li className="list-group-item flex-fill">Name</li>
-            <li className="list-group-item flex-fill">Phone</li>
-            <li className="list-group-item flex-fill">Email</li>
-            <li className="list-group-item flex-fill">DOB</li>
-        </ul>
-        <ul className="list-group-flush list-group-xl" id="title">
+
+      
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+        {props.results.map(result => (
+          <tr key={result.key}>
+            <th scope="row"><img src={result.picture.thumbnail} alt={result.name.first} /></th>
+            <td>{result.name.first} {result.name.last}</td>
+            <td>{result.phone}</td>
+            <td>{result.email}</td>
+            <td>{result.dob.date}</td>
+          </tr>
+        ))}
+    
+        </tbody>
+      </table>
+        
+        {/* <div className="container">
+          <div className="row justify-content-md-center">
+            <div className="col">
+              
+            </div>
+            <div className="col">
+              Name
+            </div>
+            <div className="col">
+              Phone
+            </div>
+            <div className="col-md-auto">
+              Email
+            </div>
+            <div className="col">
+              DOB
+            </div>
+
+
+          </div>
+
+        </div> */}
+
+        {/* <div className="container">
+        {props.results.map(result => (
+          <div className="row justify-content-md-center" key={result.key}>
+            <div className="col">
+              <img src={result.picture.thumbnail} alt={result.name.first} />
+            </div>
+            <div className="col">
+              {result.name.first} {result.name.last}
+            </div>
+            <div className="col">
+              {result.phone}
+            </div>
+            <div className="col-md-auto">
+              {result.email}
+            </div>
+            <div className="col">
+             {result.dob.date}
+            </div>
+
+
+          </div>
+
+          
+        
+        ))}
+        </div>
+         */}
+        {/* <ul className="list-group-flush list-group-xl" id="title">
             {props.results.map(result => (
                <li className="list-group-item flex-fill" key={result.id}>
                    <img src={result.picture.thumbnail} alt={result.name.first} />
@@ -20,13 +90,13 @@ function SearchResults(props) {
                    {result.email}
                 </li>
                
-            ))}
+            ))} */}
             {/* <li className="list-group-item flex-fill">{props.picture}</li>
             <li className="list-group-item flex-fill">{props.name}</li>
             <li className="list-group-item flex-fill">{props.phone}</li>
             <li className="list-group-item flex-fill">{props.email}</li>
             <li className="list-group-item flex-fill">{props.dob}</li> */}
-        </ul>
+        {/* </ul> */}
 
     </div>
   )
